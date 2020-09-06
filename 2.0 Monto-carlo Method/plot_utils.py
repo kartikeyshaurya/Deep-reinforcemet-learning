6930e8df9bcbc5eb45d4830b365d4714e263cbb9
Kartikey Shaurya <kartikeyshaurya@yahoo.com>
2020-09-06 08:31:57 +0530

    making the plotting utilis

---
+++ 2.0 Monto-carlo Method/plot_utils.py
@@ -0,0 +1,7 @@
+import numpy as np 
+from mpl_toolkits.mplot3d import Axes3D 
+import matplotlib.pyplot as plt 
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+
+
+def plot_blackjack_value(v):
